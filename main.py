293b277e329e51f4e430d99268425cf97d4bf3ac
legiordano <lucasgiordano2219@gmail.com>
2023-07-09 22:14:27 -0300

    Add image to read

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from PIL import Image
 import pandas as pd
 
-image_path = 'imagen.jpg'
+image_path = 'a.png'
 image = Image.open(image_path)
 
 pixel_data = list(image.getdata())
 
 df = pd.DataFrame(pixel_data, columns=['R', 'G', 'B'])
 
-output_path = 'imagen.parquet'
+output_path = 'a.parquet'
 df.to_parquet(output_path)
