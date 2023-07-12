5ef625b09b595c08cedeb4f0d622728250b57a30
legiordano <lucasgiordano2219@gmail.com>
2023-07-12 18:13:04 -0300

    Add function to order code

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pytesseract
 from PIL import Image
 
+def extract_text_from_image(image_path):
+    image = Image.open(image_path)
+    image = image.convert('L')
+    text = pytesseract.image_to_string(image)
+    return text
+
 image_path = 'a.png'
-
-image = Image.open(image_path)
-
-image = image.convert('L')
-
-text = pytesseract.image_to_string(image)
-
+text = extract_text_from_image(image_path)
 print(text)
